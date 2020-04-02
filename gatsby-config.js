dc05cca286aca64038891ca7f022729bc30767b7
jempurich <jempurich@gmail.com>
2020-04-02 18:37:40 +0800

    no message

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,6 +64,27 @@ module.exports = {
                     {
                         resolve: `gatsby-rehype-prismjs`,
                     },
+                    {
+                        resolve: `gatsby-plugin-google-analytics`,
+                        options: {
+                        // The property ID; the tracking code won't be generated without it
+                        trackingId: "UA-68637067-6",
+                        // Defines where to place the tracking script - `true` in the head and `false` in the body
+                        head: false,
+                        // Setting this parameter is optional
+                        anonymize: true,
+                        // Setting this parameter is also optional
+                        respectDNT: true,
+                        // Avoids sending pageview hits from custom paths
+                        exclude: ["/preview/**", "/do-not-track/me/too/"],
+                        // Delays sending pageview hits on route update (in milliseconds)
+                        pageTransitionDelay: 0,
+                        // Enables Google Optimize using your container Id
+                        sampleRate: 5,
+                        siteSpeedSampleRate: 10,
+                        cookieDomain: "purich.co",
+                        },
+                    },
                 ],
             },
         },
