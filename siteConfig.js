54180e8304b6d735c39457ba2bf5f13bdfc0ab8d
jempurich <jempurich@gmail.com>
2020-04-10 17:44:19 +0800

    update gatsby-theme-try-ghost

--- siteConfig.js
+++ siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     siteUrl: `https://purich.co`, // Site domain. Do not include a trailing slash!
 
-    postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
+    postsPerPage: 3, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
     siteTitleMeta: `ความคิด เรื่องราว ไอเดีย`, // This allows an alternative site title for meta data for pages.
     siteDescriptionMeta: `ความคิด เรื่องราว ไอเดีย`, // This allows an alternative site description for meta data for pages.
