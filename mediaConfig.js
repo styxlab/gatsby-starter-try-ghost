6ac835a07ccc2a036d011957733122e399f416e2
cws <cws@home.fritz.box>
2020-06-28 20:43:17 +0200

    reset image defaults

--- mediaConfig.js
+++ mediaConfig.js
@@ -7,8 +7,8 @@ module.exports = {
     gatsbyImageLoading: `lazy`,
 
     // Fading in transition (default: true)
-    // true negatively impacts Lighthouse LCP
-    gatsbyImageFadeIn: false,
+    // true negatively impacts Lighthouse 6 LCP score
+    gatsbyImageFadeIn: true,
 
     // External URL for serving images, videos and other media
     mediaUrl: ``,
